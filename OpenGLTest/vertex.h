4c8294801274c60db87f449d0ef6e4222d2f2bf8
Lolinonusos <89453125+Lolinonusos@users.noreply.github.com>
2023-01-17 16:56:47 +0100

    Update vertex.h

--- OpenGLTest/vertex.h
+++ OpenGLTest/vertex.h
@@ -1,14 +1,16 @@
 #ifndef VERTEX_H
 #define VERTEX_H
 
+
 #include <vector>
+#include "glm/glm.hpp"
 
 class Vertex {
 public:
 	// Will hold x, y, z coortinates, r, g, b values, and u, v coordinates
-	std::vector<float> vertices;
-
-	void addVert(float x, float y, float z, float r, float g, float b, float u, float v);
+	glm::vec3 position;
+	glm::vec3 normal;
+	glm::vec2 texCoord;
 };
 
 #endif
