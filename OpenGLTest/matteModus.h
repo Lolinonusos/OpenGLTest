1217b63537699d04f950aa297fc057fa33c1973c
Lolinonusos <89453125+Lolinonusos@users.noreply.github.com>
2023-02-14 22:11:05 +0100

    oof

--- OpenGLTest/matteModus.h
+++ OpenGLTest/matteModus.h
@@ -1,12 +1,16 @@
 #pragma once
 
 #include <vector>
+#include "blaze/math/Matrix.h"
 
 class Matrise {
 	std::vector<std::vector <float>> vectorVector[2];
 	std::vector<float> matSize;
 
 
+	blaze::DynamicMatrix<float> A;
+
+	// A
 
 	void matMultiply(Matrise) {
 
