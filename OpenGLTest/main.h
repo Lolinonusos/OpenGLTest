5c554fa373967337b20de522c1f377d68ceeab48
Lolinonusos <linus.nordbakken@gmail.com>
2023-02-16 09:11:07 +0100

    Update main.h

--- OpenGLTest/main.h
+++ OpenGLTest/main.h
@@ -1,25 +1,34 @@
 #pragma once
 
+// Base OpenGL
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stb/stb_image.h>
 
+// OpenGL matte
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+// std
 #include <iostream>
 
+// LearnOpenGL.com
 #include "shader.h"
 #include "camera.h"
 #include "model.h"
 
+// 3D programmering obliger
 #include "xyz.h"
 #include "cube.h"
 #include "graph.h"
 #include "triangleSurface.h"
 #include "interactive.h"
 
+// Ultra matte bibliotek
 #include <Eigen/Core>
+
+// Matte obliger
+#include "matteModus.h"
 
 //#include <blaze/Blaze.h>
