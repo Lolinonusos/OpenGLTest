e5656999ba2b6495dd9aeb58e8111268e92e6a18
Lolinonusos <linus.nordbakken@gmail.com>
2023-02-18 14:35:56 +0100

    Update matteModus.h

--- OpenGLTest/matteModus.h
+++ OpenGLTest/matteModus.h
@@ -1,6 +1,7 @@
 #pragma once
 
 
+#include <Eigen/Core>
 #include <Eigen/Eigen>
 //#include <vector>
 
@@ -20,9 +21,27 @@ class Matrise {
 
 	// y = A * x + e
 	// B = AT * A
-	// C = AT * y
+	// c = AT * y
 	Matrise();
-	Eigen::Matrix<float, 8, 2>;
+	Eigen::Matrix<float, 2, 8> A{ 
+		{1.0f, 2.0f},
+		{2.0f, 1.0f},
+		{3.0f, 2.5f},
+		{4.0f, 4.5f},
+		{5.0f, 3.0f},
+		{6.0f, 4.0f},
+		{7.0f, 5.0f},
+		{8.0f, 6.5f}
+	};
+
+	Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> AT = A.transpose();
+
+	Eigen::Matrix<float, 8, 1> y;
+
+	Eigen::Matrix<float, 2, 2> B;
+
+	Eigen::Matrix<float, 2, 1> c;
+
 
 	void leastSquaresMethod() {
 		for (int i = 0; i < 8; i++) {
